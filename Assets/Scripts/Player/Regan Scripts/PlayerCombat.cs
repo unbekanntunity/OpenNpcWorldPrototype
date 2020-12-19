c4e8117b97d48c2a8f80d8e56704564edac45531
ReganR <reganrai08@gmail.com>
2020-12-19 12:37:42 +0000

    PlayerCombatSystem
    Ive moddified Health script for blocking, but for attacking, i simply just added a script where the player can left click on an enemy.

---
+++ Assets/Scripts/Player/Regan Scripts/PlayerCombat.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public float attackPoint = 20;
+    public static bool isBlock;
+
+    void Update()
+    {
+        playerAttack();
+        playerBlock();
+    }
+
+    void playerAttack()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 3))
+            {
+                if (hit.transform.gameObject.CompareTag("Enemy"))
+                {
+                    hit.transform.gameObject.GetComponent<Health>().Damage(attackPoint);
+                }
+            }
+        }
+    }
+
+    void playerBlock()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            isBlock = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Q))
+        {
+            isBlock = false;
+        }
+    }
+}
