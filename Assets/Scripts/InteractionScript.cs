2010c556f970265d8ba2df1e82ea57020b51910b
Pavel418 <62073659+Pavel418@users.noreply.github.com>
2021-01-01 20:56:57 +0400

    npc-npc interaction

---
+++ Assets/Scripts/InteractionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionScript : MonoBehaviour
+{
+    public int priority;
+    public bool isTalking;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Npc"))
+        {
+            GameObject obj = other.gameObject;
+            InteractionScript script = obj.GetComponent<InteractionScript>();
+            if (!script.isTalking)
+            {
+                if (priority > script.priority)
+                {
+                    if (Random.Range(0, 100) < 21)
+                    {
+                        isTalking = true;
+                        script.isTalking = true;
+                        Debug.Log("Send request");
+                    }
+                }
+            }
+        }
+    }
+}
