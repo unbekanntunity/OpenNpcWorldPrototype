17ea32f1d21c4754a9007a73ea98a697745a7874
Pavel418 <pavel@vvv.az>
2021-01-12 18:51:04 +0400

    Stats and Attacks added

---
+++ Assets/Combat/Scripts/Attack.cs
@@ -0,0 +1,22 @@
+﻿
+public class Attack
+{
+    private readonly int _damage;
+    private readonly bool _critical;
+
+    public Attack(int dam, bool crit)
+    {
+        _damage = dam;
+        _critical = crit;
+    }
+
+    public int Damage
+    {
+        get { return _damage; }
+    }
+
+    public bool isCritical
+    {
+        get { return _critical; }
+    }
+}
---
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public Stat maxHealth;
+    public Stat currentHealth { get; private set; }
+
+    public Stat Damage;
+    public Stat Armor;
+
+    void Awake()
+    {
+        currentHealth = new Stat();
+        currentHealth.SetValue(maxHealth.GetValue());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10.0f);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        damage -= Armor.GetValue();
+
+        if (damage > 0.0f)
+            currentHealth.SetValue(currentHealth.GetValue() - damage);
+        Debug.Log(transform.name + " takes " + damage + " damage");
+
+        if (currentHealth.GetValue() <= 0.0f)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + " died");
+    }
+
+    public Stat GetArmor()
+    {
+        return Armor;
+    }
+
+    public Stat GetDamage()
+    {
+        return Damage;
+    }
+}
